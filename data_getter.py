bbb42fb838654a2d90bb54320a43a6d3d42f8117
ryan <2yan@outlook.com>
2018-04-13 10:00:21 -0700

    more

--- data_getter.py
+++ data_getter.py
@@ -3,16 +3,30 @@ import pandas_datareader as pdr
 import pandas as pd
 
 
+
+def multi_ticker_download(ticker, start, end):
+    ticker = ticker.upper()
+    if  '_FRED_' in ticker:
+        ticker = ticker.replace('_FRED_', '')
+        data = pdr.get_data_fred(ticker, start, end)
+        final = pd.DataFrame(index = pd.date_range(start, end))
+        final.index.name = ticker
+        final['Date'] = final.index
+        final['Close'] = data
+        final['Close'] = final['Close'].ffill()
+        return final
+    data = pdr.DataReader(ticker,data_source = 'iex', start =start , end =end , retry_count= 3, pause= 1)
+    return data
+
 def download_ticker(ticker, look_back):
     
     ticker = ticker.upper()
     key = ticker + ' {} '.format(int(look_back))
-    print(key)
     cache = get_cache()
     if key in cache.keys():
         return cache[key]
-    today = datetime.today()
-    data = pdr.DataReader(ticker,data_source = 'iex', start = today - timedelta(days = look_back), end = today , retry_count= 3, pause= 1)
+    today = pd.to_datetime(datetime.today().strftime('%Y-%m-%d'))
+    data = multi_ticker_download(ticker, today - timedelta(days = look_back), today )
     data = data.reset_index()
     cols = []
     for column in data.columns:
@@ -39,6 +53,7 @@ def download_tickers(tickers, look_back):
     today = pd.to_datetime(datetime.today().strftime('%Y-%B-%d'))
     final = pd.DataFrame(index = pd.date_range(today- timedelta(days = look_back), today))
     for ticker in tickers:
+        ticker = ticker.upper()
         ticker = ticker.strip()
         if '-' in ticker:
             a, b = ticker.split('-')
